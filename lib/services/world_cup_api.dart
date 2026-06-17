843a5e5098896f3e30364fef774e87a9bdb159f9
51237 <ledezguillaume2@gmail.com>
2026-06-17 11:50:02 +0200

    fetch teams from openfootball

--- lib/services/world_cup_api.dart
+++ lib/services/world_cup_api.dart
@@ -1,13 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/match.dart';
+import '../models/team.dart';
 
 class WorldCupApi {
-  static const String _url =
+  static const String _matchesUrl =
       'https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json';
 
+  static const String _teamsUrl =
+      'https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.teams.json';
+
   Future<List<Match>> fetchMatches() async {
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse(_matchesUrl));
 
     if (response.statusCode != 200) {
       throw Exception('Echec du chargement (HTTP ${response.statusCode})');
@@ -19,5 +23,19 @@ class WorldCupApi {
     return matchesJson
         .map((m) => Match.fromJson(m as Map<String, dynamic>))
         .toList();
+  }
+
+  Future<List<Team>> fetchTeams() async {
+    final response = await http.get(Uri.parse(_teamsUrl));
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        'Echec du chargement des équipes (HTTP ${response.statusCode})',
+      );
+    }
+
+    // Le fichier teams a une LISTE à la racine, pas un objet
+    final data = jsonDecode(response.body) as List;
+    return data.map((t) => Team.fromJson(t as Map<String, dynamic>)).toList();
   }
 }
